30576c0efe54d7d9d845c1800a2d0c914d3c8f5c
Louis Dorard <louis@louisdorard.com>
2024-12-11 16:39:31 +0000

    Update app

--- app.py
+++ app.py
@@ -4,26 +4,30 @@ from dash_extensions.enrich import DashProxy, html, Input, Output, State
 from dash_extensions import EventListener
 from dash.exceptions import PreventUpdate
 
-# JavaScript event(s) that we want to listen to and what properties to collect.
-event = {"event": "message", "props": ["k"]}
-
 app = DashProxy()
 
 app.layout = html.Div([
-    EventListener(logging=True, id="el"),
-    html.Div(id="log")
+    EventListener(
+        # events=[{"event": "message", "props": ["k"]}], # JavaScript event(s) that we want to listen to and what properties to collect.
+        logging=True,
+        id="el" # "el" stands for event listener
+    ),
+    html.Div(id="log", children="init")
 ])
 
 @app.callback(
     Output("log", "children"),
     Input("el", "n_events"), # the number of events fired
-    State("el", "event")) # the last event fired
+    State("el", "event"), # the last event fired
+    prevent_initial_call=True)
 def message_event(n_events, event):
-    print(n_events)
     print(event)
     if event is None:
         raise PreventUpdate()
+    data = event.data
+    if (data and data.k):
+        print("Caught event! value is " + data.k)
     return "callback"
 
 if __name__ == "__main__":
-    app.run_server()
+    app.run_server(debug=True, host="0.0.0.0", port=8050)
