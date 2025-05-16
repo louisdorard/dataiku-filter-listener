01d11b736351adf77f139780e5a62b90c1897c16
lhfudataiku <liheng.fu@dataiku.com>
2025-05-16 12:01:00 +0200

    build

--- streamlit_filter_listener/frontend/src/index.tsx
+++ streamlit_filter_listener/frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import FilterListener from "./StreamlitFilterListener";
+import FilterListener from "./StreamlitFilterListener.tsx";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
