681b77488875792c9b145d12860a15ceca0b6984
lhfudataiku <liheng.fu@dataiku.com>
2025-05-23 09:48:47 +0200

    debug

--- streamlit_filter_listener/frontend/src/index.tsx
+++ streamlit_filter_listener/frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import FilterListener from "./StreamlitFilterListener.tsx";
+import FilterListener from "./StreamlitFilterListener";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
